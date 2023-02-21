306f0a2db76ed814f489e0b4abde4b642381ba92
Glauco Leme <glaucooleme@gmail.com>
2023-02-20 22:03:11 -0300

    feat: Aula 2: introduzindo ExpressJS

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Pagina de livros',
-    '/autores': 'Lista de autores',
-    '/editora': 'Pagina de editora',
-    '/sobre': 'Informacoes sobre o projeto'
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text-plain' });
-    console.log(`URL: ${req.url}`);
-    console.log(`rota: ${rotas[req.url]}`);
-    res.end(rotas[req.url]);
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
-})
+});
