481e7335c988ab869b0bdbe65809102ee7316e29
Glauco Leme <glaucooleme@gmail.com>
2023-02-22 22:53:29 -0300

    feat(mongo): Using mongoose and best practises

---
+++ src/controllers/livrosController.js
@@ -0,0 +1,55 @@
+import livros from "../models/Livro.js";
+
+class LivroController {
+    static listarLivros = (req, res) => {
+        livros.find((err, livros) => {
+            res.status(200).json(livros);
+        })
+    }
+
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+        livros.findById(id, (err, livro) => {
+            if (err) {
+                res.status(400).send({ message: `Id do livro não localizado: ${err.message}` });
+            } else {
+                res.status(200).send(livro.toJSON());
+            }
+        })
+    }
+
+    static cadastrarLivro = (req, res) => {
+        let livro = new livros(req.body);
+        livro.save((err) => {
+            if (err) {
+                res.status(500).send({ message: `Falha ao cadastrar livro: ${err.message}` });
+            } else {
+                res.status(201).send(livro.toJSON());
+            }
+        })
+    }
+
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+        livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (!err) {
+                res.status(200).send({ message: 'Livro atualizado com sucesso!' })
+            } else {
+                res.status(500).send({ message: `Falha ao atualizar livro: ${err.message}` });
+            }
+        });
+    }
+
+    static excluirLivro = (req, res) => {
+        const id = req.params.id;
+        livros.findByIdAndDelete(id, (err) => {
+            if (err) {
+                res.status(500).send({ message: `Falha ao deletar livro: ${err.message}` });
+            } else {
+                res.status(200).send({ message: 'Livro deletado com sucesso!' })
+            }
+        })
+    }
+}
+
+export default LivroController
